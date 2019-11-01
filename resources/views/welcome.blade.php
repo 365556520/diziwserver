d031c03c47f514c88012a3b97a3fc1bd48bb26b9
365556520 <365556520@qq.com>
2019-11-01 11:45:55 +0800

    第二次测试链接

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
             @endif
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-6.0
+                    Laravel-6.0测试
                 </div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
