9567dacf4d335e29ce7de1913824070dc7caea4c
365556520 <365556520@qq.com>
2019-12-01 17:48:52 +0800

    菜单增删改查已经完成剩下优化

--- resources/views/admin/permissions/list.blade.php
+++ resources/views/admin/permissions/list.blade.php
@@ -8,7 +8,6 @@
 @section('content')
         <div >
             <script type="text/html" id="toolbarDemo">
-
             </script>
             <table class="layui-hide" id="permissions" lay-filter="permissions" lay-size="lg"></table>
             <script type="text/html" id="barDemo">
@@ -19,10 +18,19 @@
 @endsection
 @section('js')
     <script>
-        layui.use('table', function(){
-            var table = layui.table;
-            table.render({
-                elem: '#permissions'
+        var  ptable = null, treeGrid = null, tableId = 'permissions', layer = null;
+        // layui方法
+        layui.config({
+            base: '/extend/layui/extend/treeGrid/'//配置 layui 第三方扩展组件存放的基础目录
+        }).extend({
+            treeGrid: 'treeGrid' //定义该组件模块名
+        }).use(['jquery', 'treeGrid', 'layer'], function () {
+            var $ = layui.jquery;
+            treeGrid = layui.treeGrid;//很重要
+            layer = layui.layer;
+            ptable = treeGrid.render({
+                id: tableId
+                ,elem: '#' + tableId
                 ,url:'/admin/permissions/ajaxIndex'
                 ,toolbar: '#toolbarDemo' //开启头部工具栏，并为其绑定左侧模板
                 ,defaultToolbar: ['filter', 'exports', 'print', { //自定义头部工具栏右侧图标。如无需自定义，去除该参数即可
@@ -30,9 +38,19 @@
                     ,layEvent: 'LAYTABLE_TIPS'
                     ,icon: 'layui-icon-tips'
                 }]
-                ,title: '用户数据表'
-                ,height: 'full-50'
-                ,width: '1100'
+                , idField: 'id'//必須字段
+                , treeId: 'id'//树形id字段名称
+                , treeUpId: 'pid'//树形父id字段名称
+                , treeShowName: 'name'//以树形式显示的字段
+                , heightRemove: [".dHead", 10]//不计算的高度,表格设定的是固定高度，此项不生效
+                , height: '100%'
+                , isFilter: false
+                , iconOpen: false//是否显示图标【默认显示】
+                , isOpenDefault: false//节点默认是展开还是折叠【默认展开】
+                , loading: true
+                , method: 'get'
+                , isPage: false
+                , limit: 1000 //默认采用100
                 ,cols: [[
                     {type: 'checkbox', fixed: 'left'}
                     ,{field:'id', title:'ID', width:80, fixed: 'left', unresize: true, sort: true}
