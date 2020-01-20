f8e65e03411e2618a8c702cf98eb2a3991aaeae7
365556520 <365556520@qq.com>
2020-01-20 16:12:15 +0800

    优化查询功能

--- app/Http/Controllers/Api/usersdata/BusesController.php
+++ app/Http/Controllers/Api/usersdata/BusesController.php
@@ -8,6 +8,7 @@ use App\Models\AminModels\Buses\BusesRoute;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\CommonController;
 use DB;
+use phpDocumentor\Reflection\Types\Array_;
 
 class BusesController extends CommonController
 {
@@ -19,16 +20,24 @@ class BusesController extends CommonController
         $newdata = [];
         foreach ($data as $k=>$v){
             $newdata['buses_start'][$k] = $v->buses_start;
-            $newdata['buses_midway'][$k] = $v->buses_midway;
+            $newdata['buses_midway'][$k] = json_decode($v->buses_midway,true);
             $newdata['buses_end'][$k] = $v->buses_end;
         }
         //去出重复的始发地（array_unique去出数组中重复）
         $newdata['buses_start'] = array_unique($newdata['buses_start']);
         //终点和途经合并去重 (array_filter()去除数组中的空，用implode把路径数组转换成以点分割的字符串，然后在用explode以点分割转换成数组最后用array_merge把2个数组合并)
-        $newdata['buses_end'] = array_filter(array_unique(array_merge($newdata['buses_end'],explode(",", implode(",", $newdata['buses_midway'])))));
+        $lv = []; //遍历途经的地名
+        foreach($newdata['buses_midway'] as $v){
+            if($v){
+                foreach ($v as $d){
+                    array_push($lv,$d);
+                }
+            }
+        }
+        $newdata['buses_end'] = array_filter(array_merge($newdata['buses_end'],$lv));
         //所有地名
         $newdata['buses_route_name'] = array_unique(array_merge($newdata['buses_start'],$newdata['buses_end']));
-        return $this->response($newdata);
+        return $this->response($newdata,'获取地名成功',200);
     }
     /*
      * 获取该线路所有车辆first 和find都是返回给模型
@@ -43,19 +52,22 @@ class BusesController extends CommonController
     public function getBusesRouteId(Request $request){
         $buses_start = $request->buses_start;
         $buses_end =  $request->buses_end;
-        //获取线路
-        $data = BusesRoute::whereRaw('buses_start =? and buses_end = ?',[$buses_start,$buses_end])
-                            ->orWhere([['buses_start','=',$buses_start],['buses_midway','like','%'.$buses_end.'%']])
-                            ->get();
-        if($data->isEmpty()){
-            $data = BusesRoute::whereRaw('buses_end =? and buses_start = ?',[$buses_start,$buses_end])
-                ->orWhere([['buses_end','=',$buses_start],['buses_midway','like','%'.$buses_end.'%']])
+        if(isset($buses_start)&&isset($buses_end)){
+            //获取线路
+            $data = BusesRoute::whereRaw('buses_start =? and buses_end = ?',[$buses_start,$buses_end])
+                ->orWhere([['buses_start','=',$buses_start],['buses_midway','like','%'.$buses_end.'%']])
                 ->get();
+            if($data->isEmpty()){
+                $data = BusesRoute::whereRaw('buses_end =? and buses_start = ?',[$buses_start,$buses_end])
+                    ->orWhere([['buses_end','=',$buses_start],['buses_midway','like','%'.$buses_end.'%']])
+                    ->get();
+            }
+            foreach ( $data as $k=>$v){
+                $data[$k]['buses'] = $v->getBuses;
+            }
+            return $this->response($data,'车辆获取成功',200);
         }
-        foreach ( $data as $k=>$v){
-            $data[$k]['buses'] = $v->getBuses;
-        }
-        return $this->response($data);
+        return $this->response('','请输入起始地和目的地',400);
     }
     /*
      * 获取该车信息
