75f7818d01c30eef0c8739a6db4cd7a68c9a5f19
365556520 <365556520@qq.com>
2021-03-30 16:49:32 +0800

    增加首页维护

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">通知</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    维护中！稍等后
-                </div>
-            </div>
         </div>
     </div>
 </div>
