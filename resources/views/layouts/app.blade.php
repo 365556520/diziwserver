f4a0bd007036676fef43a3ccd99535686262d885
365556520 <365556520@qq.com>
2021-04-13 18:21:28 +0800

    增加首

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,74 +8,80 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+{{--
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>--}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+{{--
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    @yield('css')
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm navbar-fixed-top">
-            <div class="container">
+
+
+{{--
+
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top container  ">
+            <div class="navbar-header">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{--{{ config('app.name', 'Laravel') }}--}}
-                    <img alt="Brand" src="{{ asset('myimages/images/logo.png') }}" style="height: 60px">
+                    <img  src="{{ asset('myimages/images/logo.png')  }}" height="45px">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="{{ url('/') }}">首页 <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">游戏</a>
+                    <a class="nav-link" href="#">作品</a>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ url('/') }}">首页 <span class="sr-only">(current)</span></a></li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('登录') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                          {{--      <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('注册') }}</a>
-                                </li>--}}
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a  class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
                 </div>
             </div>
         </nav>
+--}}
+{{--
+                                    <!-- Right Side Of Navbar -->
+                                    <ul class="navbar-nav">
+                                        <!-- Authentication Links -->
+                                        @guest
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('login') }}">{{ __('登录') }}</a>
+                                            </li>
+                                            @if (Route::has('register'))
+                                          --}}{{----}}{{--      <li class="nav-item">
+                                                    <a class="nav-link" href="{{ route('register') }}">{{ __('注册') }}</a>
+                                                </li>--}}{{----}}{{--
+                                            @endif
+                                        @else
+                                            <li class="nav-item dropdown">
+                                                <a  class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                                </a>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+                                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                                       onclick="event.preventDefault();
+                                                                     document.getElementById('logout-form').submit();">
+                                                        {{ __('Logout') }}
+                                                    </a>
+
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                        @csrf
+                                                    </form>
+                                                </div>
+                                            </li>
+                                        @endguest
+                                    </ul>--}}
+
+    <main class="py-4">
+        @yield('content')
+    </main>
+
     </div>
+    @yield('js')
 </body>
 </html>
