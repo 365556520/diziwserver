00ebf66d5ce413f860e8d8f254e9a335fe2e2c44
365556520 <365556520@qq.com>
2021-06-01 10:40:10 +0800

    api登录账号没有清除问题解决了 在laravels中设置

--- app/Http/Controllers/Api/Auth/PassportController.php
+++ app/Http/Controllers/Api/Auth/PassportController.php
@@ -147,13 +147,24 @@ class PassportController extends CommonController
     //获取获取大一平台登录key一次只能用30分钟
     public function  getDayiKey(){
         $date = '';
-        $pass = Permission::where('name','xxfhgj')->first();
+        //判读当前用户的权限
+        $pass = '';
+        //判断账号属于村村通还是客运  目的是根据权限获取账号信息
+        if (\auth()->user()->givePermissionTo('cuncuntong')) {
+            $pass = Permission::where('name','xxfhgj')->first();
+        }else if (\auth()->user()->givePermissionTo('keyun')){
+            $pass = Permission::where('name','nyswxxjad')->first();
+        }
         $url = "http://123.162.189.21/gps-web/api/login.jsp?password=".$pass->guard_name."&userId=".$pass->name."&loginType=user&loginWay=interface&loginLang=zh_CN";
         $client = new \GuzzleHttp\Client();
         try {
             $res =  $client->request('POST',$url);
             $date = json_decode($res->getBody()->getContents());//把获取的json数据转换成数组
-            $date->usersid = $pass->name;
+            $date->Permission =  'api.user'; //设置账号为普通用户权限
+            if(\auth()->user()->givePermissionTo('api.admin')){ //判断当前用户是否是管理
+                $date->Permission =  'api.admin'; //设置账号为管理员权限
+            }
+            $date->usersid = $pass->name; //返回账号名字
             $this->successStatus = 200;
         } catch (RequestException $e) {
             if ($e->hasResponse()) {
